f0bf7839ba958f755d1621768aba73a45e1955cd
Naveenkumar3012 <107923289+Naveenkumar3012@users.noreply.github.com>
2022-07-29 11:59:06 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,5 +2,6 @@ public class Hello{
 public static void main(String[]args){
 System.out.println("Hello naveen");
 System.out.println("welcome to jenkins");
+  System.out.println("welcome to github integration with jenkins");
 }
 }
